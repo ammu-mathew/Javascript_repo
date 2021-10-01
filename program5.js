701b7f0a971befc01e4e697010edc46335fec314
ammu-mathew <ammumathew21@gmail.com>
2021-10-01 22:32:44 +0530

    added program5 next

---
+++ program5.js
@@ -0,0 +1,67 @@
+// Create a reminder function, which adds the given reminder to the specific day the user wants, based on the message passed.
+
+// All the inputs sentences will be in lower case.
+// Create a Map object with names of days as keys, and array of reminder strings as values.
+// Convert the Map object to an array, then JSON.stringify and return the output
+// Use the extractReminderText() given as an input to extract the reminder text from the message passed.
+// Remove the day keyword from the message. (tomorrow, today, monday etc)
+// Go through the test cases, to get an idea of expected keywords.
+// Week starts at Sunday.
+// Try out the question on Saturday, and make sure your solution works!
+function setReminder(messageArray, extractReminderText) {
+    const reminders = new Map();
+    const daysOfWeek = [
+      "sunday",
+      "monday",
+      "tuesday",
+      "wednesday",
+      "thursday",
+      "friday",
+      "saturday",
+    ];
+    daysOfWeek.map((day) => {
+      reminders.set(day, []);
+    });
+    function formatAndSet(day, keyword, currentValue, newValue) {
+      formattedNewValue = newValue.replace(keyword, "");
+      reminders.set(day, [...currentValue, formattedNewValue]);
+    }
+    function handleDayKeywords(keyword, text) {
+      const dayKeywords = {
+        everyday: (text, keyword) => {
+          daysOfWeek.map((day) => {
+            formatAndSet(day, keyword, reminders.get(day), text);
+          });
+        },
+        today: (text, keyword) => {
+          const number = new Date().getDay();
+          const today = daysOfWeek[number];
+          formatAndSet(today, keyword, reminders.get(today), text);
+        },
+        tomorrow: (text, keyword) => {
+          const number = new Date().getDay();
+          const tomorrow = number === 6 ? daysOfWeek[0] : daysOfWeek[number + 1];
+          formatAndSet(tomorrow, keyword, reminders.get(tomorrow), text);
+        },
+      };
+      dayKeywords[keyword](text, keyword);
+    }
+    for (const message of messageArray) {
+      const reminderText = extractReminderText(message);
+      const arrayOfWords = reminderText.split(" ");
+      const dayKeywords = ["everyday", "today", "tomorrow"];
+      for (const value of arrayOfWords) {
+        if (daysOfWeek.includes(value)) {
+          if (reminders.has(value)) {
+            formatAndSet(value, value, reminders.get(value), reminderText);
+            break;
+          }
+        } else if (dayKeywords.includes(value)) {
+          handleDayKeywords(value, reminderText);
+          break;
+        }
+      }
+    }
+    return JSON.stringify([...reminders]);
+  }
+  
